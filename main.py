262499026fbd850a406e9eae961102217867fe00
camehu <camehu@gmail.com>
2022-03-03 11:41:53 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
-app = FastAPI ()
-
-app.mount ("/static", StaticFiles ( directory="static"), name="static" )
+app = FastAPI()
 
 templates = Jinja2Templates ( directory="templates" )
 
