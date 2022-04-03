ac9d4ac0821165e82a264bb8a356855fa6cf519c
camehu <camehu@gmail.com>
2022-04-02 21:49:20 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,16 +2,23 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+import roteadores
 
 app = FastAPI()
 
 templates = Jinja2Templates ( directory="templates" )
-#app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get ("/", response_class=HTMLResponse )
 async def index(request: Request) :
-    return templates.TemplateResponse ("index.html", {"request" : request} )
+    return templates.TemplateResponse ("index.html", {"request" : request } )
+
+
+@app.get ("/lista", response_class=HTMLResponse )
+async def index(request: Request):
+    lista = roteadores.Roteadores()
+    return templates.TemplateResponse ("lista.html", {"request" : request, 'exibirlista': lista})
 
 
 if __name__ == '__mail__':
