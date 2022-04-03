ecc0630df541b3b9f575ad2b1fadf961c88bb278
camehu <camehu@gmail.com>
2022-04-02 22:09:31 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import roteadores
 app = FastAPI()
 
 templates = Jinja2Templates ( directory="templates" )
-app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get ("/", response_class=HTMLResponse )
